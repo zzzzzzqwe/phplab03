0dc9dbc05d6f496e8252270fa91a42c0ea5b654c
zzzzzzqwe <dima.gachayev@gmail.com>
2025-03-10 21:52:58 +0200

    minor changes

--- index2.php
+++ index2.php
@@ -18,7 +18,6 @@ $files = scandir($dir);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Галерея изображений</title>
     <style>
         body {
             font-family: Arial, sans-serif;
